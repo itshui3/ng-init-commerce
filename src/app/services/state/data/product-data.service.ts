31bdf34142b2fc8b5c0b3ecad0f197c6d08fdb40
itshui3 <xuhuizhu369@gmail.com>
2023-04-05 14:20:31 -0700

    products state!

--- src/app/services/state/data/product-data.service.ts
+++ src/app/services/state/data/product-data.service.ts
@@ -1,9 +1,39 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import {
+  ProductAPIService,
+  ProductResponse,
+} from '../../api/productAPI.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductDataService {
+  private lastFetchedProducts: Date | undefined;
+  private service: ProductAPIService;
 
-  constructor() { }
+  private products: BehaviorSubject<ProductResponse> = new BehaviorSubject(
+    [] as ProductResponse
+  );
+  public products$ = this.products.asObservable();
+  // given that products is receiving .next pushes,
+  // how does products.asObservable() become informed of changes?
+
+  constructor(service: ProductAPIService) {
+    this.service = service;
+  }
+
+  getProducts(): Observable<ProductResponse> {
+    const oneHourInMs = 60 * 60 * 1000;
+    if (
+      !this.lastFetchedProducts ||
+      new Date().getTime() - this.lastFetchedProducts.getTime() > oneHourInMs
+    ) {
+      this.service.fetchSomeProducts().subscribe(products => {
+        this.products.next(products);
+      });
+    }
+  
+    return this.products$;
+  }
 }
--- src/app/ui/product/product-list/product-list.component.ts
+++ src/app/ui/product/product-list/product-list.component.ts
@@ -1,29 +1,20 @@
-import { Component } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import {
   ProductAPIService,
   ProductResponse,
 } from '../../../services/api/productAPI.service';
+import { ProductDataService } from '../../../services/state/data/product-data.service';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css'],
 })
-export class ProductListComponent {
-  products: ProductResponse = [];
-  service: Subscription = new Subscription();
-
-  constructor(private productsService: ProductAPIService) {}
-
+export class ProductListComponent implements OnInit {
+  public products$: Observable<ProductResponse> | undefined;
+  constructor(private productState: ProductDataService) {}
   ngOnInit() {
-    this.service = this.productsService.fetchSomeProducts().subscribe({
-      next: (products) => (this.products = products),
-      error: console.error,
-    });
-  }
-
-  ngOnDestroy() {
-    this.service.unsubscribe();
+    this.products$ = this.productState.getProducts();
   }
 }
